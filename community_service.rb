87fc5c6a9ae32d2dc32124d880eba875bf4403d3
Marvin Laucher <marv070@gmail.com>
2015-10-26 15:19:29 -0400

    community service code

--- community_service.rb
+++ community_service.rb
@@ -1,47 +1,84 @@
 require 'date'
 require 'csv'
 
+
 def comm_serv_file_RnW
-#probaitoners = CSV.read("Community_service_scrubbed.csv")
-data = []
-#comm_serv_org = File.open("Community_service_scrubbed.csv","r")
-filename = "community_service_update.csv"
-file_new= File.open(filename,"w") 
+#input is a csv, output is new .csv file
+  newfile = File.open('community_service_update.csv', "w")
+    
+    CSV.foreach ('Community_service_scrubbed.csv') do |row|
+      if row[0] == "Name"
+        header = row.join(",")
+        newfile.print header << ",Hours Remaining,Days Remaining\n"
+      else
+        hours = hours_remaining(row)
+        days = days_left(row, Date.today)
+        risk = risk_factor(hours, days,row)
+        row[7] = risk
+         row[6] = "" if row[6]=="1.1.70"
+        days = "Data Missing" if days <-10000 
+        hours = "Data Missing" if row[4]== nil or row[5]== nil 
+        new = (row.push(hours, days)).join(",")
+        newfile.print "#{new}\n"
+      end
+    end
+  
+  newfile.close
+end
+
+def hours_remaining(array)
+#input is array, output is a float
+    assigned = array[4].to_f
+    completed = array[5].to_f
+  
+    remaining_hours = assigned - completed
  
-    CSV.foreach('Community_service_scrubbed.csv', :headers=>true) do |row|
-data << row
-hours_given = row[4].to_f
-hours_completed = row[5].to_f
-if row[6] == nil
-	row[6] = "01.01.1960"
-end	
-
-max_date = row[6].split(".")
-formatted_max_date = max_date[1].to_s + "-" + max_date[0].to_s + "-" + max_date[2].to_s
-puts formatted_max_date 
-todays_date = Date.today
-days_remaining = Date.parse(formatted_max_date) - todays_date
-hours_remaining =  hours_given - hours_completed
-
-def risk_factor(hours_remaining,days_remaining)
-hours_remaining.to_f / days_remaining.to_f
 end
 
-def hours_remaining(assigned,completed)
-assigned.to_f - complete.to_f
-
+def clean_date(row)
+#input is .csv array, output is changed string in position 6, if applicable
+  if row[6] == nil || (row[6].include? "?")
+    row[6] = "1.1.70"
+  end
+  
+  row
 end
 
-def days_remaining(max_days,today_date)
-max_days.to_f - today_date.to_f
-	 
+def days_left(row, date)
+#input is an array of  strings coming from the .csv, output is an integer
+  row_with_revised_date = clean_date(row)
+  today = (date).to_s #format YYYY/MM/DD
+  
+  max_date = row[6] #formatted MM/DD/YYYY/
+  
+  modified_max_date_array = max_date.split(".")
+    
+  modified_max_date = modified_max_date_array[2] + "." + modified_max_date_array[0] + "." + modified_max_date_array[1]
+      
+  days = (Date.parse(modified_max_date)) - (Date.parse(today))
+  
+  days_left = days.to_i
 end
 
-warning =  risk_factor(hours_remaining,days_remaining)
 
- file_new.puts row.to_csv.chomp + "," + hours_remaining.to_s + "," + warning.round(3).to_s 
 
-end
+
+def risk_factor(hours, days,row)
+  #input two variables, output is a single variables
+     days = -1 if days == 0
+    risk = hours/days.to_f
+    if row[4] == nil or row[5] == nil or row[6] == "1.1.70"
+      risk_factor = "Data missing"
+    elsif risk == 0 
+      risk_factor = "Hours Completed"
+    elsif risk <0
+      risk_factor = "High Risk"
+    else
+    risk = (hours / days).round(2)
+    
+    end
+  
+    
 end
 
 comm_serv_file_RnW
